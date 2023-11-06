e48fadae6d7568ef3e05c7936e22cac09e1a9105
arnaudjudge <arnaud.judge@usherbrooke.ca>
2023-11-06 13:28:11 -0500

    add rotations to tta for patchless nnunet

--- ascent/train.py
+++ ascent/train.py
@@ -116,7 +116,7 @@ class AscentTrainer(ABC):
         if cfg.get("test"):
             log.info("Starting testing!")
             trainer_conf = cfg.trainer
-            trainer_conf['accelerator'] = 'auto'
+            trainer_conf['strategy'] = 'auto'
             trainer_conf['devices'] = 1
             trainer: Trainer = hydra.utils.instantiate(trainer_conf, callbacks=callbacks, logger=logger)
             if cfg.get("best_model"):
